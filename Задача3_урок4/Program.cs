2db55278997876e4d25f5e89d9e8f4df9cdde232
Anya <mango08609@mail.ru>
2023-02-09 20:49:31 +0300

    решение задачи

---
+++ Задача3_урок4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру.
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine()!);
+int res = 0;
+int count = 1;
+
+while(count <= a)
+{
+    res = count * count * count;
+    if(res % 2 == 0)
+    {
+        Console.WriteLine(res);
+    }
+    count++;
+}
