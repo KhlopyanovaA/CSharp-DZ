4538a97cb4f654c91b03951459c980e923c17c6a
Anya <mango08609@mail.ru>
2023-02-25 13:45:01 +0300

    решение

---
+++ Задача066/Program.cs
@@ -0,0 +1,18 @@
+﻿// 
+
+
+int A(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1,A(n,m-1));
+}
+Console.WriteLine("введите число m ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число n ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(A(n, m));
